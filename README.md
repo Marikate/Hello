4df33f27a7d5461bab220ea338b2be10eae0cc91
Ame <53622807+Marikate@users.noreply.github.com>
2022-04-02 21:58:11 -0400

    Gitsum 1.0
    First archives on github

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello
- 
+1. Index html
+2. Hello.js
+ 2.1 Soma;
---
+++ hello.js
@@ -0,0 +1,7 @@
+//Meu primeiro script no Github
+
+var a = 1
+var b = 1;
+gitsum = a+b;
+
+window.document.write("Gitsum: ",gitsum);
